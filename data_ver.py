adc320a619c0d5f27678148adaa0f6b238e2764a
developerayeshaahmad-netizen <developer.ayeshaahmad@gmail.com>
2026-06-29 00:40:15 +0500

    Day 2: Learned veriables and data types

---
+++ data_ver.py
@@ -0,0 +1,63 @@
+
+              # TASK 1
+"""Practice reassigning variables from integer to string to None"""
+
+current_score = 100
+print(current_score)
+#Game finished 
+current_score = "Game over"
+print(current_score)
+#Reset the score
+current_score = None
+print(current_score)
+
+print("\n\n")
+
+                     # TASK 2
+#  Code challenge exploring booleans versus string data types
+
+is_logged_in = True
+Statue_text = "True"
+
+print("Is_logged_in type = " , type(is_logged_in))
+print("Status_text type = " , type(Statue_text))
+
+print(is_logged_in == Statue_text)
+
+"""
+First veriable is not equal to second one . 
+First one is boolean and second one is string
+"""
+
+print("\n\n")
+                #Task 3
+#Calculate total bill using integers and floats
+item_price = 15.74
+quantity = 5
+
+Total_bill = item_price * quantity
+
+print(Total_bill)
+
+print("\n\n")
+                #Task 4
+#Compare empty string and None type behavior
+    
+username = ""
+email = None
+
+print(username == email)
+
+'''
+Empty string is the string containing no cheracte 
+on the other hand the none type is like no veriable 
+exist 
+so they cannot be same  string != None
+'''
+
+
+
+
+
+
+                          
