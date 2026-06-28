bb6c5acccb983e9451c6863f5012b71c006378c4
developerayeshaahmad-netizen <developer.ayeshaahmad@gmail.com>
2026-06-28 18:39:18 +0500

    Day 2:practise comment and escape sequence

---
+++ day2.py
@@ -0,0 +1,34 @@
+
+                           # Day two of learning Python 
+                           #Learning comments and escape sequences
+
+
+# TASK 1 
+
+print("Learning python:\n -Day 1:  Basics\n -Day 2:  Escape Sequences\n \"Practice makes perfect!\"")
+
+# TASk 2
+
+"""
+DEVELPOR LOG:
+DATE: 28 june 2026
+AUTHOR: AYESHA AHMAD
+PROJECT: DAY 2, LEARNING COMMENTS AND ESCAPE SEQUENCE
+"""
+print("\n \n")
+
+#A dialog using escape sequence
+print("The teacher said,\"Make sure your project is saved in C:\\Users\\Python\\\"\nI replied, \"Yes sir! I'll push it to Git right away.\"")
+
+print("\n \n")
+
+#TASK 3
+
+
+
+print("=========================================")
+print("| NAME:\t\t[AYESHA AHMAD]\t|") # Two tabs after NAME
+print("| PROGRESS:\tDAY 2\t\t\t|") # One tab after PROGRESS, extra tabs at the end
+print("| STATUS:\t\"Coding\"\t\t|") # One tab after STATUS, extra tabs at the end
+print("=========================================")
+
